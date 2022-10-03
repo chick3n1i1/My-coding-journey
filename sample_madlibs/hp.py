9e12da89700853f709c3b916cde0de80e11274c3
chick3n1i1 <105393860+chick3n1i1@users.noreply.github.com>
2022-10-03 14:44:42 -0400

    Madlibs updated
    Added Star Wars madlib

--- sample_madlibs/hp.py
+++ sample_madlibs/hp.py
@@ -28,7 +28,7 @@ The bang was like a cannon blast, and the {adj2} flames that erupted between the
 at the dead center of the circle they had been treading, marked the point where the \
 {noun_plural} collided. Harry saw Voldemort’s {adj3} jet meet his own spell, saw the Elder Wand \
 fly high, dark against the sunrise, spinning across the enchanted ceiling like the \
-head of Nagini, spinning through the air toward the master it would not {verb}, who had \
+head of Nagini, spinning through the air toward the master it would not {verb1}, who had \
 come to take full possession of it at last. And Harry, with the unerring skill of a Seeker, \
 caught the {noun4} in his free hand as Voldemort fell backward, arms splayed, the slit pupils \
 of the {adj4} {body_part2} rolling upward. Tom Riddle hit the floor with a mundane finality, his body \
@@ -36,5 +36,4 @@ feeble and shrunken, the white hands empty, the snakelike face vacant and unknow
 was dead, {verb_past2} by his own rebounding {noun5}, and Harry stood with two wands in his hands, \
 staring down at his enemy’s {noun6}."
 
-
-print(madlib)
+    print(madlib)
---
+++ sample_madlibs/starwars.py
@@ -0,0 +1,27 @@
+adj1 = input("Adjective: ")
+noun = input("Noun: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+noun_place = input("Place: ")
+adj4 = input("Adjective: ")
+adj5 = input("Adjective: ")
+vechile_plural = input("Vechile (plural): ")
+plural_noun = input("Noun (plural): ")
+noun2 = input("Noun: ")
+plural_noun2 = input("Noun (plural): ")
+adj6 = input("Adjective: ")
+noun3 = input("Noun: ")
+verb1 = input("Verb: ")
+adj7 = input("Adjective: ")
+verb2 = input("Verb: ")
+plural_noun3 = input("Noun (plural):")
+adj8 = input("Adjective: ")
+verb3 = input("Verb: ")
+adj9 = input("Adjective: ")
+
+madlib = f'Star Wars is a {adj1} {noun} of {adj2} versus {adj3} in a {noun_place} far far away. \n\
+    There are {adj4} battles between {adj5} {vechile_plural} and duels with {plural_noun} called {noun3}. \n\
+        {plural_noun2} called droids are helpers and {plural_noun2} to the heroes. A {adj6} power called The {noun2} {verb1} people to do {adj7} things, like {verb2} {plural_noun3}. \n\
+            The Jedi use the Force for the {adj8} and the Sith {verb3} it for the {adj9} side.'
+
+print(madlib)
