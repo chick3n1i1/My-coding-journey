67c6153c4519575976a3e6bc41e454e1edf3aa23
chick3n1i1 <105393860+chick3n1i1@users.noreply.github.com>
2023-01-25 18:05:50 -0500

    update

---
+++ tic-tac-toe.py
@@ -0,0 +1,12 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        # Letter is x or o
+        self.letter = letter
+
+        # we want all players to get their next move
+        def get_move(self, game):
+            pass
