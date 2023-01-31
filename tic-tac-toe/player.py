51094f3af10f9b0712951f60ab730c734e835ca2
chick3n1i1 <105393860+chick3n1i1@users.noreply.github.com>
2023-01-31 18:47:40 -0500

    Tic-Tac-Toe
    recreated both player.py and game.py
    
    still unable to test.
    Issue with importing "Player" class into game.py

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -4,10 +4,10 @@ import random
 
 class Player:
     def __init__(self, letter):
-        # letter is x or o
+        #letter is X or O
         self.letter = letter
 
-    # we want all players to get their next move given a game
+        # we want all players to get their next move given a game
     def get_move(self, game):
         pass
 
@@ -17,7 +17,9 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        # get a random valid spot for our next move
+        square = random.choice(game.available_moves())
+        return square
 
 
 class HumanPlayer(Player):
@@ -25,4 +27,19 @@ class HumanPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # we're going to check that this is a correect value by trying to cast
+            # it to an integer, and if it's not, then we say its invalid
+            # if that spot is not available on the board, we also say its invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True  # if thse are successful, then yay!
+            except ValueError:
+                print('Invalid square. Try again.')
+
+        return val
