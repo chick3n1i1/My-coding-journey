85450609c5890478043a18738514ce95f456ef7d
chick3n1i1 <105393860+chick3n1i1@users.noreply.github.com>
2023-02-09 19:13:48 -0500

    Tic-Tac-Toe final
    Completed Tic-Tac-Toe game. Added unbeatable AI.

--- tic-tac-toe/player.py
+++ tic-tac-toe/player.py
@@ -2,24 +2,12 @@ import math
 import random
 
 
-class Player:
+class Player():
     def __init__(self, letter):
-        #letter is X or O
         self.letter = letter
 
-        # we want all players to get their next move given a game
     def get_move(self, game):
         pass
-
-
-class RandomComputerPlayer(Player):
-    def __init__(self, letter):
-        super().__init__(letter)
-
-    def get_move(self, game):
-        # get a random valid spot for our next move
-        square = random.choice(game.available_moves())
-        return square
 
 
 class HumanPlayer(Player):
@@ -30,16 +18,69 @@ class HumanPlayer(Player):
         valid_square = False
         val = None
         while not valid_square:
-            square = input(self.letter + '\'s turn. Input move (0-8):')
-            # we're going to check that this is a correect value by trying to cast
-            # it to an integer, and if it's not, then we say its invalid
-            # if that spot is not available on the board, we also say its invalid
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
             try:
                 val = int(square)
                 if val not in game.available_moves():
                     raise ValueError
-                valid_square = True  # if thse are successful, then yay!
+                valid_square = True
             except ValueError:
                 print('Invalid square. Try again.')
-
         return val
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+
+class SmartComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter  # yourself
+        other_player = 'O' if player == 'X' else 'X'
+
+        # first we want to check if the previous move is a winner
+        if state.current_winner == other_player:
+            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                state.num_empty_squares() + 1)}
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            # each score should maximize
+            best = {'position': None, 'score': -math.inf}
+        else:
+            # each score should minimize
+            best = {'position': None, 'score': math.inf}
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            # simulate a game after making that move
+            sim_score = self.minimax(state, other_player)
+
+            # undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            # this represents the move optimal next move
+            sim_score['position'] = possible_move
+
+            if player == max_player:  # X is max player
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
